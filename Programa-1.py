c328cbd20ca453ffc0aca2ed3180e5de2ef9956a
oscarlucas22 <98739924+oscarlucas22@users.noreply.github.com>
2022-05-27 19:06:37 +0200

    Programa 1

--- Programa-1.py
+++ Programa-1.py
@@ -12,14 +12,11 @@ url = url_base + "soccer/" + "countries"
 r = requests.get(url,params = payload)
 if r.status_code == 200:
     datos=r.json()
-    print(json.dumps(datos,indent=4,sort_keys=True))
-    respuesta = input("¿Quieres ver una vista simplificada? (S/N): ")
-    if respuesta == "S":
-        id = input("¿Que id quieres que muestre información?: ")
-        var = input("¿Que quieres que muestre?: ")
-        print(json.dumps(datos.get("data").get(id).get(var),indent=4,sort_keys=True))
-    else:
-        print("Ok")
+    for resultado in datos["data"]:
+        print(resultado)
+    id = input("¿Que id quieres que muestre información?: ")
+    var = input("¿Que quieres que muestre? (continent, contry_code, contry_id, name): ")
+    print(json.dumps(datos.get("data").get(id).get(var),indent=4,sort_keys=True))
 else:
     print("Error en la API")
     print(r.status_code)
